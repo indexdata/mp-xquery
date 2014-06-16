50ec8f654a469d68dddd3741fac7c2a93095db42
Adam Dickmeiss <adam@indexdata.dk>
2014-06-16 13:59:45 +0000

    Add IDMETA + mkdist.sh

---
+++ mkdist.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+. ./IDMETA
+set -x
+git log >ChangeLog
+git archive --format=tar --prefix=$NAME-$VERSION/ HEAD > $NAME-$VERSION.tar
+tar xf $NAME-$VERSION.tar
+cp ChangeLog $NAME-$VERSION
+cd $NAME-$VERSION
+rm -fr debian
+cd ..
+tar cfz $NAME-$VERSION.tar.gz $NAME-$VERSION
+rm $NAME-$VERSION.tar
+rm -r $NAME-$VERSION
+exit 0
+
+
