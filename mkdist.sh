96be0721627f5c01b81616239cdb566bd33300dd
Adam Dickmeiss <adam@indexdata.dk>
2015-05-27 10:42:06 +0200

    Make documentation part of dist

--- mkdist.sh
+++ mkdist.sh
@@ -5,9 +5,12 @@ git log >ChangeLog
 git archive --format=tar --prefix=$NAME-$VERSION/ HEAD > $NAME-$VERSION.tar
 tar xf $NAME-$VERSION.tar
 cp ChangeLog $NAME-$VERSION
+cp -r doc/common $NAME-$VERSION/doc
 cd $NAME-$VERSION
 rm -fr debian
-cd ..
+cd doc
+make
+cd ../..
 tar cfz $NAME-$VERSION.tar.gz $NAME-$VERSION
 rm $NAME-$VERSION.tar
 rm -r $NAME-$VERSION
